a52f6b5be63d2ea834c8f736cfc9b08395111f8a
ismaelresp <7013638+ismaelresp@users.noreply.github.com>
2024-02-14 15:40:13 +0100

    Created ClassSimilarity model in alignment app

--- alignment/models.py
+++ alignment/models.py
@@ -6,3 +6,10 @@ class AlignmentConsensus(models.Model):
     slug = models.SlugField(max_length=100, unique=True)
     alignment = models.BinaryField()
     gn_consensus = models.BinaryField(blank=True) # Store conservation calculation for each GN
+
+class ClassSimilarity(models.Model):
+    ProteinFamily1 = models.ForeignKey('protein.ProteinFamily', on_delete=models.CASCADE, related_name='class_similarity_protein_family1')
+    ProteinFamily2 = models.ForeignKey('protein.ProteinFamily', on_delete=models.CASCADE, related_name='class_similarity_protein_family2')
+    Similarity = models.IntegerField(null=False)
+    Protein1 = models.ForeignKey('protein.Protein', on_delete=models.CASCADE, related_name='class_similarity_protein1')
+    Protein2 = models.ForeignKey('protein.Protein', on_delete=models.CASCADE, related_name='class_similarity_protein2')
